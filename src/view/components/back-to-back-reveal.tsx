412bdcbdda7b162da6b713102fc084e64e451dd3
bartu <bartukutlu@rightsoft.co.uk>
2025-05-22 15:44:11 +0300

    potential scroll fix

--- src/view/components/back-to-back-reveal.tsx
+++ src/view/components/back-to-back-reveal.tsx
@@ -6,51 +6,65 @@ import BuiltInEmail from "./sliding-sections/built-in-email";
 import SeoReady from "./sliding-sections/seo-ready";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import { useWindowSize } from "hamo";
 gsap.registerPlugin(ScrollTrigger);
 export default function BackToBackReveal({
 }: {
     }) {
     const $ref = useRef<HTMLDivElement>(null);
+    const { width } = useWindowSize();
     useEffect(() => {
-        const items = gsap.utils.toArray(".reveal-item");
-        const mm = gsap.matchMedia();
-        mm.add("(min-width: 1024px)", () => {
-            if ($ref.current) {
-                const tl = gsap.timeline(
-                    {
-                        scrollTrigger: {
-                            trigger: $ref.current,
-                            start: "center center",
-                            end: "+=1800px",
-                            pin: true,
-                            scrub: true,
+        if (!width || width < 1024) return;
+        let timeline: gsap.core.Timeline | null = null;
+        const handleRefresh = () => {
+            // timeline?.scrollTrigger?.refresh();
+            timeline?.revert();
+        }
+        setTimeout(() => {
+            const items = gsap.utils.toArray(".reveal-item");
+            const mm = gsap.matchMedia();
+            mm.add("(min-width: 1024px)", () => {
+                if ($ref.current) {
+                    timeline = gsap.timeline(
+                        {
+                            scrollTrigger: {
+                                trigger: $ref.current,
+                                start: "center center",
+                                end: "+=1800px",
+                                pin: true,
+                                scrub: true,
+                            }
                         }
-                    }
-                );
-                items.forEach((item: any, index) => {
-                    const itemTl = gsap.timeline();
-                    const a = gsap.from(item, {
-                        opacity: 0,
-                        duration: 1,
+                    );
+                    items.forEach((item: any, index) => {
+                        const itemTl = gsap.timeline();
+                        const a = gsap.from(item, {
+                            opacity: 0,
+                            duration: 1,
+                        });
+                        const b = gsap.to(item, {
+                            opacity: 0,
+                            duration: 1,
+                            delay: 1.5,
+                        });
+                        if (index == 0) {
+                            itemTl.add(b);
+                        } else if (index == items.length - 1) {
+                            itemTl.add(a);
+                        } else {
+                            itemTl.add(a, 0);
+                            itemTl.add(b, 1);
+                        }
+                        timeline?.add(itemTl);
                     });
-                    const b = gsap.to(item, {
-                        opacity: 0,
-                        duration: 1,
-                        delay: 1.5,
-                    });
-                    if (index == 0) {
-                        itemTl.add(b);
-                    } else if (index == items.length - 1) {
-                        itemTl.add(a);
-                    } else {
-                        itemTl.add(a, 0);
-                        itemTl.add(b, 1);
-                    }
-                    tl.add(itemTl);
-                });
-            }
-        });
-    }, [])
+                }
+            });
+        }, 500);
+        window.addEventListener("resize", handleRefresh);
+        return () => {
+            window.removeEventListener("resize", handleRefresh);
+        };
+    }, [width])
     return <div ref={$ref} className="relative h-[100vh] lg:hidden">
         <RevealItem> <InteractiveWebsiteSection /></RevealItem>
         <RevealItem>      <SwitchBetweenTemplates /></RevealItem>
