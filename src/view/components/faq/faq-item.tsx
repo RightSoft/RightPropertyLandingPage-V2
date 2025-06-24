4bc30b764bee91ec243d216c23dd8e9e99930e2a
bartu <bartukutlu@rightsoft.co.uk>
2025-06-24 09:52:22 +0300

    faq item heading change

--- src/view/components/faq/faq-item.tsx
+++ src/view/components/faq/faq-item.tsx
@@ -34,7 +34,7 @@ export default function FaqItem({ question, answer }: { question: string | React
             <path d="M12 13L12 21M12 21L15 18M12 21L9 18" stroke="#1F3147" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
         </svg>
         <div>
-            <h5 className="font-geist text-24 lg:text-18 font-medium leading-[140%] tracking-[0%]">{question}</h5>
+            <h3 className="font-geist text-24 lg:text-18 font-medium leading-[140%] tracking-[0%]">{question}</h3>
             {/* {isOpen && <div className="mb-[0.75rem]"></div>} */}
             {<div ref={answerRef} className="h-0 mt-[0.75rem] overflow-hidden font-geist text-18 lg:text-base font-normal leading-[140%] tracking-[0%]">{answer}</div>}
         </div>
