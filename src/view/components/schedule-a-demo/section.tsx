320fda2386c5def51354e555f45a4c225d0a772d
bartu <bartukutlu@rightsoft.co.uk>
2025-06-25 13:12:37 +0300

    schedule demo heading update

--- src/view/components/schedule-a-demo/section.tsx
+++ src/view/components/schedule-a-demo/section.tsx
@@ -7,9 +7,9 @@ export default function ScheduleADemoSection() {
                 <p className="mb-[0.625rem] font-geist text-base lg:text-14 font-normal leading-[150%] tracking-[0.04em] uppercase">
                     Let's take a seat
                 </p>
-                <h2 className="mb-10 font-general-sans text-80 lg:text-40 font-semibold leading-[100%] tracking-[0%]">
+                <h3 className="mb-10 font-general-sans text-80 lg:text-40 font-semibold leading-[100%] tracking-[0%]">
                     Schedule <br />a demo meeting
-                </h2>
+                </h3>
                 <p className="mb-6 font-geist text-24 lg:text-18 font-normal leading-[140%] tracking-[0] w-[26.25rem] lg:w-full">
                     We'd be happy to meet and discuss how we can transform your business.
             </p>
